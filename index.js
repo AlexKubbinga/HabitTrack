d50c4f256418f9fb655d6c3c96e9d66d86bc7676
Alex Kubbinga <63915427+AlexKubbinga@users.noreply.github.com>
2022-09-07 18:20:03 +0200

    authenticated with Oura: personal details

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 require('dotenv').config('./.env');
 
 const PAT = process.env.PAT;
-console.log(PAT);
+//
+
+var myHeaders = new Headers();
+
+myHeaders.append('Authorization', `Bearer ${PAT}`);
+myHeaders.get('Authorization');
+console.log(myHeaders.get('Authorization'));
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+};
+
+fetch(
+  'https://api.ouraring.com/v2/usercollection/personal_info?start_date=2021-11-01&end_date=2021-12-01',
+  requestOptions
+)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.log('error', error));
